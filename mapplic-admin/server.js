2e26bea5b35a3012d6bad21aec2ab3dbe1e8fc3b
Daniel Arvizu <daniel.m.arvizu@gmail.com>
2024-02-16 18:34:43 -0600

    Os added and implementes on server.js

--- mapplic-admin/server.js
+++ mapplic-admin/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import os from 'os';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,4 +14,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(9000);
+const PORT = 9000;
+app.listen(PORT, () => {
+  const networkInterfaces = os.networkInterfaces();
+  for (const key in networkInterfaces) {
+    const iface = networkInterfaces[key];
+    for (let i = 0; i < iface.length; i++) {
+      if (iface[i].family === 'IPv4' && !iface[i].internal) {
+        console.log(`La aplicación está disponible en http://${iface[i].address}:${PORT}`);
+      }
+    }
+  }
+});
