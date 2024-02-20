8608cdb87cba5e74ed1a23b2c29e5d67886ff28a
Daniel Arvizu <daniel.m.arvizu@gmail.com>
2024-02-19 20:57:59 -0600

    test

--- mapplic-admin/dist/save_express.js
+++ mapplic-admin/dist/save_express.js
@@ -373,9 +373,13 @@ function formatFileName(fileName) {
 /*USERS*/
 // Ruta para autenticación y generación de token
 app.post('/login', (req, res) => {
+
+  console.log('Directorio de trabajo=========>');
+  console.log(process.cwd());
+
   const { username, password } = req.body;
 
-  const usersFilePath = 'users.json';
+  const usersFilePath = './users.json';
   let users = [];
   
   try {
